8380e46d8482c98c89c8f663d153dd50dcfe29a2
VanDany <psr10701@students.ephec.be>
2021-08-07 22:28:49 +0200

    Update Get ajax

--- Models.Client/Data/Transaction.cs
+++ Models.Client/Data/Transaction.cs
@@ -10,11 +10,13 @@ namespace Models.Client.Data
     {
         public int Id { get; private set; }
         public int UserAccountId { get; set; }
+        public string AccountName { get; set; }
         public DateTime DateTransact { get; private set; }
         public string Description { get; set; }
         public bool ExpenseOrIncome { get; private set; }
         public double Amount { get; set; }
         public int CategoryId { get; set; }
+        public string Name { get; set; }
         public Transaction(int userAccountId, string description, bool expenseOrIncome, double amount, int categoryId)
         {
             UserAccountId = userAccountId;
@@ -22,6 +24,16 @@ namespace Models.Client.Data
             ExpenseOrIncome = expenseOrIncome;
             Amount = amount;
             CategoryId = categoryId;
+        }
+        public Transaction(int userAccountId, string accountName, string description, bool expenseOrIncome, double amount, int categoryId, string name)
+        {
+            UserAccountId = userAccountId;
+            AccountName = accountName;
+            Description = description;
+            ExpenseOrIncome = expenseOrIncome;
+            Amount = amount;
+            CategoryId = categoryId;
+            Name = name;
         }
         internal Transaction(int id, int userAccountId, DateTime dateTransact, string description, bool expenseOrIncome, double amount, int categoryId)
             : this(userAccountId, description, expenseOrIncome, amount, categoryId)
--- MoneyManager.Website/Controllers/TransactionController.cs
+++ MoneyManager.Website/Controllers/TransactionController.cs
@@ -15,12 +15,14 @@ namespace MoneyManager.Website.Controllers
     {
         private readonly ITransactionRepository _transactionRepository;
         private readonly IAccountRepository _accountRepository;
+        private readonly ICategoryRepository _categoryRepository;
         private readonly ISessionManager _sessionManager;
-        public TransactionController(ITransactionRepository transactionRepository, ISessionManager sessionManager, IAccountRepository accountRepository)
+        public TransactionController(ITransactionRepository transactionRepository, ISessionManager sessionManager, IAccountRepository accountRepository, ICategoryRepository categoryRepository)
         {
             _transactionRepository = transactionRepository;
             _accountRepository = accountRepository;
             _sessionManager = sessionManager;
+            _categoryRepository = categoryRepository;
         }
         public IActionResult Index()
         {
@@ -36,12 +38,77 @@ namespace MoneyManager.Website.Controllers
             if (id==0)
             {
                 IEnumerable<Transaction> transactions = _transactionRepository.Get();
-                return Json(transactions);
+                IEnumerable<Account> accounts = _accountRepository.Get();
+                IEnumerable<Category> categories = _categoryRepository.Get();
+                
+                var innerJoin = transactions.Join(
+                    accounts,
+                    transactions => transactions.UserAccountId,
+                    accounts => accounts.Id,
+                    (transactions, accounts) => new
+                    {
+                        Transaction = transactions,
+                        Id = transactions.Id,
+                        UserAccountId = transactions.UserAccountId,
+                        UserAccount = accounts.Description,
+                        DateTransact = transactions.DateTransact,
+                        Description = transactions.Description,
+                        ExpenseOrIncome = transactions.ExpenseOrIncome,
+                        Amount = transactions.Amount,
+                        CategoryId = transactions.CategoryId
+                    }).Join(categories,
+                    transactions => transactions.CategoryId,
+                    categories => categories.Id,
+                    (transactions, categories) => new
+                    {
+                        Id = transactions.Id,
+                        UserAccountId = transactions.UserAccountId,
+                        UserAccount = transactions.UserAccount,
+                        DateTransact = transactions.DateTransact,
+                        Description = transactions.Description,
+                        ExpenseOrIncome = transactions.ExpenseOrIncome,
+                        Amount = transactions.Amount,
+                        CategoryId = transactions.CategoryId,
+                        Name = categories.Name
+                    });
+                return Json(innerJoin);
             }
             else
             {
                 IEnumerable<Transaction> transactions = _transactionRepository.Get();
-                return Json(transactions);
+                IEnumerable<Account> accounts = _accountRepository.Get();
+                IEnumerable<Category> categories = _categoryRepository.Get();
+                var innerJoin = transactions.Join(
+                    accounts,
+                    transactions => transactions.UserAccountId,
+                    accounts => accounts.Id,
+                    (transactions, accounts) => new
+                    {
+                        Transaction = transactions,
+                        Id = transactions.Id,
+                        UserAccountId = transactions.UserAccountId,
+                        UserAccount = accounts.Description,
+                        DateTransact = transactions.DateTransact,
+                        Description = transactions.Description,
+                        ExpenseOrIncome = transactions.ExpenseOrIncome,
+                        Amount = transactions.Amount,
+                        CategoryId = transactions.CategoryId
+                    }).Join(categories,
+                    transactions => transactions.CategoryId,
+                    categories => categories.Id,
+                    (transactions, categories) => new
+                    {
+                        Id = transactions.Id,
+                        UserAccountId = transactions.UserAccountId,
+                        UserAccount = transactions.UserAccount,
+                        DateTransact = transactions.DateTransact,
+                        Description = transactions.Description,
+                        ExpenseOrIncome = transactions.ExpenseOrIncome,
+                        Amount = transactions.Amount,
+                        CategoryId = transactions.CategoryId,
+                        Name = categories.Name
+                    }).Where(f => f.UserAccountId == id); 
+                return Json(innerJoin);
             }
             
         }
