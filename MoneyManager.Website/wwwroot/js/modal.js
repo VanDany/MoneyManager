c6bc6f3bee4fd0e30e82dbb5dcb76411745d5db2
VanDany <psr10701@students.ephec.be>
2021-08-08 00:46:25 +0200

    add Popup newTransact

---
+++ MoneyManager.Website/wwwroot/js/modal.js
@@ -0,0 +1,26 @@
+﻿const openModalbuttons = document.querySelectorAll('[data-transactWindow-target]');
+const closeModalbuttons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+openModalbuttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector('#transactWindow');
+        openModal(modal);
+    })
+})
+
+closeModalbuttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('#transactWindow');
+        closeModal(modal);
+    })
+})
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
