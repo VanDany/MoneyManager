7dfd9e69744e0b8677cfba3dd872aba64809be5a
VanDany <psr10701@students.ephec.be>
2021-08-15 22:56:54 +0200

    update modal css

--- MoneyManager.Website/wwwroot/js/modal.js
+++ MoneyManager.Website/wwwroot/js/modal.js
@@ -19,3 +19,49 @@ function closeModal(modal) {
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
+
+//Delay to hide the first minimization of the window "créer une nouvelle transaction"
+function DisplayPopup() {
+    var millisecondsToWait = 500;
+    setTimeout(function () {
+        let doc = document.getElementById("transactWindow");
+        doc.style.display = "block";
+    }, millisecondsToWait);
+}
+//open window "créer une nouvelle transaction"
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+    $.ajax({
+        url: '/Transaction/GetCategories',
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            var element = document.getElementById("Categories");
+            while (element.firstChild) {
+                element.removeChild(element.lastChild);
+            }
+            if (response.length === 0) {
+                $("Categories").append("<option>Categories not found!</option>");
+            }
+            else {
+                response.forEach(function (Category) {
+                    addCategoryIntoSelect(Category.name, Category.id);
+                });
+            }
+        },
+        failure: function (response) {
+            alert(response);
+        }
+    });
+    function addCategoryIntoSelect(Name, Id) {
+        let option = document.createElement("option");
+        option.text = Name;
+        option.value = Id;
+        var element = document.getElementById("Categories");
+        element.appendChild(option);
+        console.log(option.text);
+        console.log(option.value);
+    }
+}
