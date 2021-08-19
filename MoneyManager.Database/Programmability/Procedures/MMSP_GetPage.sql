3909c0a74f549dde2763ede7522e598e22c37b20
VanDany <psr10701@students.ephec.be>
2021-08-19 21:10:05 +0200

    Update csproj

--- MoneyManager.Database/Programmability/Procedures/MMSP_GetPage.sql
+++ MoneyManager.Database/Programmability/Procedures/MMSP_GetPage.sql
@@ -4,11 +4,9 @@
 @UserId INT
 AS
 BEGIN
-
 SELECT T.Id, UserAccountId, DateTransact, T.[Description], ExpenseOrIncome, Amount, CategoryId FROM [Transaction] T 
 INNER JOIN UserAccount U ON T.UserAccountId = U.Id WHERE U.UserId = @UserId
 ORDER BY DateTransact DESC
 OFFSET (@PageNumber-1)*@RowsOfPage ROWS
 FETCH NEXT @RowsOfPage ROWS ONLY
-
 END
